5f44e95e0d901fd11fade7e50c0625a934e047fc
Young Yunzhao <fly_with24@163.com>
2020-07-21 22:11:39 +0800

    remove-duplicates-from-sorted-array II

---
+++ src/leetcode/Leetcode80.java
@@ -0,0 +1,32 @@
+package leetcode;
+
+/**
+ * https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array-ii/
+ */
+public class Leetcode80 {
+    public int removeDuplicates(int[] nums) {
+
+        int i = 1;
+        int count = 1;//重复项出现次数
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[j] == nums[j - 1]) {
+                //计数加1
+                count++;
+            } else {
+                count = 1;
+            }
+            if (count <= 2) {
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+        return i;
+    }
+}
+
+class Test {
+    public static void main(String[] args) {
+        Leetcode80 leetcode = new Leetcode80();
+        System.out.println(leetcode.removeDuplicates(new int[]{0, 0, 1, 1, 1, 1, 2, 3, 3}));
+    }
+}
