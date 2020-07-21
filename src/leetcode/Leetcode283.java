57373ebeb67873510ebd34fac3a67ea76ae27d43
Young Yunzhao <fly_with24@163.com>
2020-07-21 22:37:52 +0800

    move-zeroes

---
+++ src/leetcode/Leetcode283.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+/**
+ * https://leetcode-cn.com/problems/move-zeroes/
+ */
+public class Leetcode283 {
+    public void moveZeroes(int[] nums) {
+        int j = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                j++;
+            }
+        }
+    }
+}
