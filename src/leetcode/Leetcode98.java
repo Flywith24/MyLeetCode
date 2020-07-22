f86236147cbbaea2eee0c7e0bc387d0c8b4ea430
Young Yunzhao <fly_with24@163.com>
2020-07-22 23:41:00 +0800

    validate-binary-search-tree

---
+++ src/leetcode/Leetcode98.java
@@ -0,0 +1,36 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * https://leetcode-cn.com/problems/validate-binary-search-tree/
+ */
+public class Leetcode98 {
+
+    boolean isValid = true;
+    private List<Integer> list = new ArrayList<>();
+
+    public boolean isValidBST(TreeNode root) {
+        helper(root);
+        return isValid;
+    }
+
+    private void helper(TreeNode root) {
+        if (root == null || isValid == false) return;
+        helper(root.left);
+        if (list.size() != 0 && root.val <= list.get(list.size() - 1)) isValid = false;
+        list.add(root.val);
+        helper(root.right);
+    }
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
