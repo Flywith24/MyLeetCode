56973b93eaea727fafd3b57ff6d8d8508a622c72
Young Yunzhao <fly_with24@163.com>
2020-07-22 22:02:09 +0800

    n-ary-tree-preorder-traversal

---
+++ src/leetcode/Leetcode589.java
@@ -0,0 +1,42 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * https://leetcode-cn.com/problems/n-ary-tree-preorder-traversal/
+ */
+public class Leetcode589 {
+
+    private final List<Integer> result = new ArrayList<>();
+
+    public List<Integer> preorder(Node root) {
+        helper(root);
+        return result;
+    }
+
+    private void helper(Node root) {
+        if (root == null) return;
+        result.add(root.val);
+        for (Node child : root.children) {
+            helper(child);
+        }
+    }
+
+    static class Node {
+        public int val;
+        public List<Node> children;
+
+        public Node() {
+        }
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, List<Node> _children) {
+            val = _val;
+            children = _children;
+        }
+    }
+}
