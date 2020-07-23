6da39c4db451dad98dab0b25199dd1b8cf200d7d
fly_with24 <fly_with24@163.com>
2020-07-23 21:20:36 +0800

    palindrome-number

---
+++ src/leetcode/Leetcode9.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+/**
+ * https://leetcode-cn.com/problems/palindrome-number/
+ */
+public class Leetcode9 {
+    public boolean isPalindrome(int x) {
+        String s = String.valueOf(x);
+        int left = 0;
+        int right = s.length() - 1;
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
