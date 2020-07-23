9053cad7b490ef0bdd4fcb23a6da9ee908c69d5c
Young Yunzhao <fly_with24@163.com>
2020-07-24 00:09:25 +0800

    valid-palindrome

--- src/leetcode/Leetcode125.java
+++ src/leetcode/Leetcode125.java
@@ -1,14 +1,35 @@
 package leetcode;
 
+/**
+ * https://leetcode-cn.com/problems/valid-palindrome/
+ */
 public class Leetcode125 {
-    public boolean isPalindrome(String s) {
+    public boolean isPalindromeTowPoint(String s) {
         String lowercase = s.toLowerCase();
         int left = 0;
-        int right = s.length() - 1;
+        int right = lowercase.length() - 1;
 
         while (left < right) {
-            if (left<right) {
+            while (left < right && !Character.isLetterOrDigit(lowercase.charAt(left))) {
+                left++;
+            }
+            while (left < right && !Character.isLetterOrDigit(lowercase.charAt(right))) {
+                right--;
+            }
+            if (lowercase.charAt(left) != lowercase.charAt(right)) return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    public boolean isPalindromeReserve(String s) {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if (Character.isLetterOrDigit(s.charAt(i))) {
+                result.append(Character.toLowerCase(s.charAt(i)));
             }
         }
+        return result.toString().equals(result.reverse().toString());
     }
 }
