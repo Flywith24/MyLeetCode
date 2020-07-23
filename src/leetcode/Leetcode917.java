9619cc61e5aa95fc7249dc87829bbf4c69f41478
Young Yunzhao <fly_with24@163.com>
2020-07-24 00:11:45 +0800

    reverse-only-letters

--- src/leetcode/Leetcode917.java
+++ src/leetcode/Leetcode917.java
@@ -1,33 +1,28 @@
 package leetcode;
 
+/**
+ * https://leetcode-cn.com/problems/reverse-only-letters/
+ */
 public class Leetcode917 {
+
     public String reverseOnlyLetters(String s) {
-        s = "a-bC-dEf-ghIj";
         char[] array = s.toCharArray();
         int left = 0;
         int right = s.length() - 1;
         while (left < right) {
-            while ()
-            if (isLetter(array[left]) && isLetter(array[right])) {
-                char temp = array[right];
-                array[right] = array[left];
-                array[left] = temp;
-            }
+            //跳过非法字符
+            while (left < right && !Character.isLetter(array[left])) left++;
+            while (left < right && !Character.isLetter(array[right])) right--;
+
+            //交换
+            char temp = array[right];
+            array[right] = array[left];
+            array[left] = temp;
+
+            //挪动指针
             left++;
             right--;
         }
-
-        String s1 = new String(array);
-        System.out.println(s1);
-        return s1;
-    }
-
-    boolean isLetter(char c) {
-        return Character.isLetter(c);
-    }
-
-    public static void main(String[] args) {
-        Leetcode917 leetcode917 = new Leetcode917();
-        leetcode917.reverseOnlyLetters("");
+        return new String(array);
     }
 }
