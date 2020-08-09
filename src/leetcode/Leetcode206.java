8dea4c83cb9e75011ef1d8cf91933cf3fbd39f2a
Flywith24 <fly_with24@163.com>
2020-08-09 12:29:24 +0800

    reverse-linked-list

---
+++ src/leetcode/Leetcode206.java
@@ -0,0 +1,28 @@
+package leetcode;
+
+/**
+ * https://leetcode-cn.com/problems/reverse-linked-list/
+ */
+public class Leetcode206 {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode reverseList(ListNode head) {
+        //双指针
+        ListNode pre = null;
+        ListNode cur = head;
+        while (cur != null) {
+            ListNode temp = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = temp;
+        }
+        return pre;
+    }
+}
