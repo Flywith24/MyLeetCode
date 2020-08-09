fa1d954619f5cf6051581180f561b20f80a6b257
Flywith24 <fly_with24@163.com>
2020-08-09 20:43:47 +0800

    plus-one

---
+++ src/leetcode/Leetcode66.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+/**
+ * https://leetcode-cn.com/problems/plus-one/
+ */
+public class Leetcode66 {
+    public int[] plusOne(int[] digits) {
+        int add = 0;
+        for (int i = digits.length - 1; i >= 0; i--) {
+            int temp = i == digits.length - 1 ? digits[i] + 1 + add : digits[i] + add;
+            digits[i] = temp % 10;
+            add = temp / 10;
+        }
+        if (add > 0) {
+            int[] result = new int[digits.length + 1];
+            result[0] = 1;
+            System.arraycopy(digits, 0, result, 1, digits.length);
+            return result;
+        } else {
+            return digits;
+        }
+    }
+}
