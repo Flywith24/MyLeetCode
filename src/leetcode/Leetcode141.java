8afc14e35005be182f8a3e2c8fe984db1d8c56c7
Flywith24 <fly_with24@163.com>
2020-08-09 16:51:02 +0800

    linked-list-cycle

--- src/leetcode/Leetcode141.java
+++ src/leetcode/Leetcode141.java
@@ -14,15 +14,13 @@ public class Leetcode141 {
     }
 
     public boolean hasCycle(ListNode head) {
-        if (head == null || head.next == null) return false;
         ListNode slow = head;
-        ListNode fast = head.next;
-
-        while (slow != fast) {
-            if (fast == null || fast.next == null) return false;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
+            if (slow == fast) return true;
         }
-        return true;
+        return false;
     }
 }
