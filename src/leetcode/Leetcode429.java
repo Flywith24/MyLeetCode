f9155962b2c31989c9b6ee26025c14669e08ba5d
Flywith24 <fly_with24@163.com>
2020-08-12 23:37:46 +0800

    n-ary-tree-level-order-traversal

---
+++ src/leetcode/Leetcode429.java
@@ -0,0 +1,47 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * https://leetcode-cn.com/problems/n-ary-tree-level-order-traversal/
+ */
+public class Leetcode429 {
+
+    public List<List<Integer>> levelOrder(Node root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) return result;
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> temp = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                Node current = queue.poll();
+                temp.add(current.val);
+                queue.addAll(current.children);
+            }
+            result.add(temp);
+        }
+        return result;
+    }
+
+    private static class Node {
+        public int val;
+        public List<Node> children;
+
+        public Node() {
+        }
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, List<Node> _children) {
+            val = _val;
+            children = _children;
+        }
+    }
+}
