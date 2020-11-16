a5ef9211bd54759135a0bea3ab17815dd15cf716
Flywith24 <fly_with24@163.com>
2020-11-16 22:08:05 +0800

    odd-even-linked-list

---
+++ src/leetcode/Leetcode328.java
@@ -0,0 +1,34 @@
+package leetcode;
+
+/**
+ * https://leetcode-cn.com/problems/odd-even-linked-list/
+ */
+public class Leetcode328 {
+    private static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode oddEvenList(ListNode head) {
+        if (head == null) return null;
+        ListNode evenHead = head.next;
+        ListNode even = evenHead, odd = head;
+        while (even != null && even.next != null) {
+            odd.next = even.next;
+            odd = odd.next;
+            even.next = odd.next;
+            even = even.next;
+        }
+        odd.next = evenHead;
+        return head;
+    }
+}
