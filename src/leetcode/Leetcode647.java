ed6916c839b2d651b4bbcec0961a7d773a6956b9
Flywith24 <fly_with24@163.com>
2020-11-21 13:28:57 +0800

    palindromic-substrings

--- src/leetcode/Leetcode647.java
+++ src/leetcode/Leetcode647.java
@@ -4,28 +4,24 @@ package leetcode;
  * https://leetcode-cn.com/problems/palindromic-substrings/
  */
 public class Leetcode647 {
+    //找到中心点，两边扩散
+    //核心在如何找中心点，以及中心点有几个
+    //如果以一个字符为中心点，那么有 n 个
+    //但只考虑一个字符的情况会漏掉 abba 的情况
+    //因此中心可能为一个字符或两个字符，那么个数为 2 * n - 1
     public int countSubstrings(String s) {
-        int result = s.length();
-        int i, j;
-        for (i = 0; i < s.length(); i++) {
-            j = i + 1;
-            while (j < s.length()) {
-                if (isReverse(i, j, s)) result++;
-                j++;
-            }
-        }
-        return result;
-    }
+        int ans = 0;
+        for (int center = 0; center < 2 * s.length() - 1; ++center) {
+            int left = center / 2;
+            int right = left + center % 2;
 
-    private boolean isReverse(int i, int j, String s) {
-        while (i < j) {
-            if (s.charAt(i) != s.charAt(j)) {
-                return false;
-            } else {
-                i++;
-                j--;
+            while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+                ans++;
+                left--;
+                right++;
             }
         }
-        return true;
+
+        return ans;
     }
 }
